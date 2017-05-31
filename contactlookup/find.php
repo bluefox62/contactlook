6d5668ca3147517d733d26bbc3d30396b02897f3
bluefox62 <brett.riegert@smail.rasmussen.edu>
2017-05-30 19:36:11 -0500

    c4

--- contactlookup/find.php
+++ contactlookup/find.php
@@ -2,6 +2,7 @@
 		<head>
 	<title>Contact lookup</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="styles/custom.css" />
 <link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
 <link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
 <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
@@ -13,18 +14,34 @@
 		<div id="page" data-role="page" data-theme="b" >
 	<div data-role="header" data-theme="b">
 <h1>
-	Contact Lookup
+	Contact lookup
 		</h1>	</div>
 				<div data-role="content">
 
-		               <form method="post" action="find.php">
-                  <table width="400" border="0" cellspacing="1" cellpadding="2">
-                     <tr>
-                        <td width="100">Input First Name</td>
-                        <td><input name="fname" type="text" id="fname"></td>
-                     </tr>
-               </form>
-	</div>
+
+					<?php
+					include 'config.php';
+					include 'opendb.php';
+
+					$fname = (isset($_POST['fname'])    ? $_POST['fname']   : '');
+
+					$sql= "SELECT fname,
+					FROM location
+					WHERE fname  LIMIT 1";
+					$result = mysqli_query($conn, $sql);
+
+					if (mysqli_num_rows($result) > 0) {
+					    // output data of each row
+					    while($row = mysqli_fetch_assoc($result)) {
+					        echo "First Name: " . $row["fname"]. "<br><hr>";
+					    }
+					} else {
+					    echo "0 results";
+					}
+
+					mysqli_close($conn);
+
+					?>
 
 				<div data-role="footer" data-theme="b">
 	  <h4>Brett Riegert &copy; 2017</h4>
